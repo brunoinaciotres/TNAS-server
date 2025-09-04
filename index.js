e36bd4d51170091deaf88e9ee8ac3e26fb919421
Bruno Tres <btres76@gmail.com>
2025-09-04 20:17:53 -0300

    secret update, this is confidential, ok

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ import CategoryRouter from './routes/CategoryRoutes.js'
 import cors from 'cors'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: 'tnas-nine.vercel.app', // front hospedado na Vercel
+  methods: ['GET','POST','PUT','DELETE'],
+}));
 app.use(express.json())
 
 app.use('/document', DocumentRouter)
