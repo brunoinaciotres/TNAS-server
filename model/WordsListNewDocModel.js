dc95993644ff842443b34bd7e32cb0d117da52ae
Bruno Tres <btres76@gmail.com>
2026-03-07 00:34:34 -0300

    sync

--- model/WordsListNewDocModel.js
+++ model/WordsListNewDocModel.js
@@ -1,40 +1,47 @@
 import * as db from './index.js'
 
-class WordsListNewDocModel{
+class WordsListNewDocModel {
 
-    async getAllWords(){
+    async getAllWords() {
         try {
-            const query = "SELECT id, word, category_id from words_list_new_doc"
+            const query = `SELECT 
+            w.id,
+                w.word,
+                w.category_id,
+                c.nome AS category_name
+            FROM words_list_new_doc w
+            JOIN categorias c 
+            ON w.category_id = c.id`
             const res = await db.query(query)
             return res.rows
         }
-        catch(e){
+        catch (e) {
             console.log(e)
             throw new Error("Erro: " + e)
         }
     }
 
-    async insertNewWord(word, categoryId){
+    async insertNewWord(word, categoryId) {
         try {
             const query = "INSERT INTO words_list_new_doc (word,category_id) values ($1,$2)"
             const values = [word, categoryId]
             const res = await db.query(query, values)
             return res
         }
-        catch(e){
+        catch (e) {
             console.log(e)
             throw new Error("Erro: " + e.message)
         }
     }
 
-    async updateWord(id, word, categoryId){
+    async updateWord(id, word, categoryId) {
         try {
             const query = "UPDATE words_list_new_doc SET word=$1, category_id = $2 WHERE id=$3 RETURNING *"
             const values = [word, categoryId, id]
             const res = await db.query(query, values)
             return res.rows[0]
         }
-        catch(e){
+        catch (e) {
             console.log(e)
             throw new Error("Erro: " + e.message)
         }
